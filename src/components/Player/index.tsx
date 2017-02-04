7715f878d8b1c3f04f3bbdd37ced04b4c879070c
anil0906 <asharmaanil@gmail.com>
2017-02-05 00:07:45 +1100

    Player component updated with card component center aligned

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -11,7 +11,6 @@ interface IPlayerProps {
     timerDuration?: number;
 }
 export default (props: IPlayerProps) => {
-    let playerStyle : any, scoreStyle: any, handStyle: any, profileStyle: any;
     let width = 250;
     const {
         isTwoPlayerGame,
@@ -20,52 +19,53 @@ export default (props: IPlayerProps) => {
         position
     } = props;
     const isVertical = position === 'left' || position === 'right';
-    playerStyle = {
+    const styles = {
+      baseStyle: {
         display: 'flex',
-        flexDirection: isVertical ? 'column' :'row',
         justifyContent: 'center',
         alignItems: 'center',
-        width: isVertical ? width/3 + 'px' : width + 'px',
-        height: isVertical ? width + 'px': width/3 + 'px',
-        border: '5px solid silver',
-        margin: '25px'
-    }
-    scoreStyle = {
-        width: width/3 + 'px',
-        height: width/3 + 'px' ,
-        fontSize: isTwoPlayerGame ? '20px' : '30px',
-        border: 'none',
-        borderBottom:  isVertical ? '5px solid silver' : 'none',
-        borderRight: !isVertical ? '5px solid silver' : 'none'
-    }
-    profileStyle = {
-        position: 'relative',
         width: width/3 + 'px',
         height: width/3 + 'px' ,
         border: 'none',
-        borderBottom: isVertical ? '5px solid silver' : 'none',
-        borderRight: !isVertical ? '5px solid silver' : 'none'
-    }
-    handStyle = {
-        width: width/3 + 'px',
-        height: width/3 + 'px'
+      },
+      scoreStyle : {
+          fontSize: isTwoPlayerGame ? '20px' : '30px',
+          borderBottom:  isVertical ? '5px solid silver' : 'none',
+          borderRight: !isVertical ? '5px solid silver' : 'none'
+      },
+      profileStyle : {
+          position: 'relative',
+          borderBottom: isVertical ? '5px solid silver' : 'none',
+          borderRight: !isVertical ? '5px solid silver' : 'none'
+      },
+      playerStyle : {
+          display: 'flex',
+          flexDirection: isVertical ? 'column' :'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: isVertical ? width/3 + 'px' : width + 'px',
+          height: isVertical ? width + 'px': width/3 + 'px',
+          border: '5px solid silver',
+          margin: '25px'
+      }
+
     }
     const profileName: any = {
         position: 'absolute'
     }
     const score = isTwoPlayerGame ? playerScore + '/' + teamScore : teamScore + '';
-    return <div style={playerStyle as any}>
-        <div style={scoreStyle as any}>{score}</div>
-        <div style={profileStyle}>
+    return <div style={styles.playerStyle as any}>
+        <div style={{...styles.baseStyle, ...styles.scoreStyle} as any}>{score}</div>
+        <div style={{...styles.baseStyle, ...styles.profileStyle} as any}>
             <div style={profileName}></div>
 
         </div>
-        <div style={handStyle as any}>
+        <div style={styles.baseStyle as any}>
         <Card
         visible={true}
         number={2}
         type={'spades'}
-        style={{height: (width / 3) -10 + 'px',width: (68/99) * (width/3) }} 
+        style={{height: (width / 3) -10 + 'px',width: (68/99) * (width/3) }}
         />
 
 
